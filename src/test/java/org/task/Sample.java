3b2288ad36d09b8b26c1156ca6d172777d36284d
Sethulakshmibabu <sethulakshmib21@gmail.com>
2021-04-14 22:15:08 +0530

    sethu method creation

--- src/test/java/org/task/Sample.java
+++ src/test/java/org/task/Sample.java
@@ -10,4 +10,13 @@ private void method2() {
 private void method3() {
 	System.out.println("Method3");
 }
+private void method7() {
+	System.out.println("Method7");
+}
+private void method8() {
+	System.out.println("Method8");
+}
+private void method9() {
+	System.out.println("Method9");
+}
 }
